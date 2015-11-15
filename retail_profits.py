87666136ac1b7e519f744685b6227ab1c5eaf0a5
Varsha Bhat <varsha.n.bhat@gmail.com>
2015-11-15 15:16:08 +0800

    Changed code

--- retail_profits.py
+++ retail_profits.py
@@ -3,19 +3,18 @@
 Supply - x, Demand - D. 
 D is a uniform distribution between [80, 140]
 Profit = 0.6 * x when x <= D, 
-       = 0.6 * x -  0.4 * (x - D) when x > D 
+       = 0.6 * D -  0.4 * (x - D) when x > D 
 Calcuate the supply to maximize the expected profit
 """
 
 import random
-
 def retail_profit(min_D, max_D, Profit, Loss, iter):
 	profit_dict = {}
-	for i in range(min_D, max_D):
+	for i in range(min_D, max_D+1):
 		sum_profit = 0
 		count = 0
 		while count<=iter:
-			D = random.randint(min_D, max_D)
+			D = random.randint(min_D, max_D+1)
 			if D>=i:
 				profit = Profit*i
 			else:
@@ -23,12 +22,12 @@ def retail_profit(min_D, max_D, Profit, Loss, iter):
 			sum_profit = sum_profit+profit
 			count = count+1
 		profit_dict[i] = round(sum_profit/count, 3)
-	avg_prof = []
-	for key in profit_dict:
-		avg_prof.append(profit_dict[key])
+	#avg_prof = []
+	#for key in profit_dict:
+		#avg_prof.append(profit_dict[key])
 	return max(profit_dict, key = profit_dict.get)
 
-print(retail_profit(80, 140, 0.60, 0.40, 1000))	
+print(retail_profit(80, 140, 0.60, 0.40, 10000))	
 
 
 
