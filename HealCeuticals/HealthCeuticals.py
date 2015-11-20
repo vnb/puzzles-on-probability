4c3133f0d975cf3605aff9796f9de6e0f59a5422
Varsha Bhat <varsha.n.bhat@gmail.com>
2015-11-21 00:42:15 +0800

    #First Draft

---
+++ HealCeuticals/HealthCeuticals.py
@@ -0,0 +1,59 @@
+""" HealthCeuticals sells an antibioics which has no seasonality in demand.Avg demand is 5,000 units. 
+Expiration cost - $50/unit, Air Frieght(in case of extra supply) - 150$. 
+When Demand = Stocked, operating cost is 0. 
+Historical Data		
+10000,6000,10000,8000,7000,5000,5000,5000,3000,2000,6000,5000,6000,3000,5000,4000,5000,4000,4000,3000,3000,3000,4000,3000,8000,3000,5000,2000,4000,5000,7000,6000,7000,8000,1000,5000
+"""
+
+import random
+import csv
+import matplotlib.pyplot as plt
+
+file_name = 'HealthCeuticals.csv'
+file_select = open(file_name)
+data = csv.reader(file_select)
+indicator = "black"
+Historical_Data = []
+
+for row in data:
+	print(row)
+	if row[0] == "Month":
+		indicator = "yellow"
+	if indicator == "yellow" and row[0] != "Month" and row[0] != "":
+		Historical_Data.append(row[1])
+
+
+for row in data:
+	print(row)
+# 	if row[0] == "Expiration Cost":
+# 		expiration_cost = row[1]
+
+
+# for row in data:
+# 	if row[0] == "Air Frieght":
+# 		air_frieght = row[1]
+
+
+# print(expiration_cost, air_frieght) 
+
+
+def demand(historical_data):
+	""" This function generates random demand values from historical Historical_Data """
+	demand_rand = []
+	min_value = 0
+	max_value = len(historical_data)
+	for i in range(1, max_value):
+		j = random.randint(0, max_value-1)
+		demand_rand.append(historical_data[j])
+	return demand_rand 
+# #Need to write exception when historical_data is empty, duration is empty
+def avg_demand(rand_demand):
+	sum_demand = 0
+	for demand in rand_demand:
+		sum_demand += int(demand)
+	return sum_demand/len(rand_demand)
+random_demand = demand(Historical_Data)
+
+print(random_demand)
+print(avg_demand(random_demand))
+#print(Historical_Data[-1])
