bd9fd8c6b4985494b26337f3f305631ad4f9df1f
Varsha Bhat <varsha.n.bhat@gmail.com>
2015-11-21 12:04:09 +0800

    First Draft

--- HealCeuticals/HealthCeuticals.py
+++ HealCeuticals/HealthCeuticals.py
@@ -16,27 +16,25 @@ indicator = "black"
 Historical_Data = []
 
 for row in data:
-	print(row)
 	if row[0] == "Month":
 		indicator = "yellow"
 	if indicator == "yellow" and row[0] != "Month" and row[0] != "":
 		Historical_Data.append(row[1])
 
+file_name = 'HealthCeuticals.csv'
+file_select = open(file_name)
+data = csv.reader(file_select)
+
 
 for row in data:
 	print(row)
-# 	if row[0] == "Expiration Cost":
-# 		expiration_cost = row[1]
+	if row[0] == "Expiration Cost":
+		expiration_cost = row[1]
+	if row[0] == "Air Freight":
+		air_freight = row[1]	
+print(expiration_cost, air_freight)
 
-
-# for row in data:
-# 	if row[0] == "Air Frieght":
-# 		air_frieght = row[1]
-
-
-# print(expiration_cost, air_frieght) 
-
-
+print(Historical_Data)
 def demand(historical_data):
 	""" This function generates random demand values from historical Historical_Data """
 	demand_rand = []
@@ -47,13 +45,33 @@ def demand(historical_data):
 		demand_rand.append(historical_data[j])
 	return demand_rand 
 # #Need to write exception when historical_data is empty, duration is empty
+
 def avg_demand(rand_demand):
 	sum_demand = 0
 	for demand in rand_demand:
 		sum_demand += int(demand)
-	return sum_demand/len(rand_demand)
+	return int(sum_demand/len(rand_demand)
 random_demand = demand(Historical_Data)
+
+
 
 print(random_demand)
 print(avg_demand(random_demand))
-#print(Historical_Data[-1])
+
+# cost = []
+# amt_stocked = []
+# usr_demand = []
+# ##Amount Stocked
+# for i in range(min(Historical_Data), max(Historical_Data)):
+# 	demnd = avg_demand(demand(Historical_Data))
+# 	amt_stocked.append(i)
+# 	usr_demand.append(demand)
+
+# 	if i == demnd:
+# 		cost.append(0)
+# 	elif i > demnd:
+# 		cost.append((i-demnd)*50)
+# 	else:
+# 		cost.append((demnd-i)*150)
+# print(cost,amt_stocked,usr_demand)
+
--- retail_profits.py
+++ retail_profits.py
@@ -8,6 +8,7 @@ Calculate the supply to maximize the expected profit
 
 import random
 import matplotlib.pyplot as plt
+<<<<<<< Updated upstream
 
 
 class Retailer(object):
@@ -54,4 +55,49 @@ if __name__ == "__main__":
     print r.maximize_profit(80, 140)
 
 
+=======
+>>>>>>> Stashed changes
 
+
+class Retailer(object):
+
+    def __init__(self, unit_profit, unit_loss):
+        self.unit_profit = unit_profit
+        self.unit_loss= unit_loss
+
+    def plot_retail_profits(self, profits):
+
+        x_data = [key for key in profits]
+        y_data = [profits[key] for key in profits]
+
+        # begin plotting
+        fig = plt.figure(figsize = (10, 10))
+        axis = fig.add_subplot(1, 1, 1)
+        axis.scatter(x_data, y_data, color = "blue", marker = "o")
+        axis.set_title("Retail Profits")
+        axis.set_xlabel("Supply")
+        axis.set_ylabel("Profit")
+        fig.savefig("profits.png")
+
+    def profit_func(self, supply, demand):
+        if supply <= demand:
+            return self.unit_profit * supply
+        else:
+            return self.unit_profit * demand - self.unit_loss * (supply - demand)
+
+    def maximize_profit(self, min_demand, max_demand, iterations = 10000):
+        profits = {}
+        for i in range(min_demand, max_demand+1):
+            sum_profit = 0
+            for j in range(0, iterations + 1):
+                demand = random.randint(min_demand, max_demand+1)
+                sum_profit +=  self.profit_func(i, demand)
+            profits[i] = (sum_profit*1.0)/iterations
+
+        self.plot_retail_profits(profits)
+        return max(profits, key = profits.get)
+
+
+if __name__ == "__main__":
+    r = Retailer(0.6, 0.4)
+    print(r.maximize_profit(80, 140))
