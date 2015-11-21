86246fa0b65f770f34b314a28ac570308ab9cb7a
Varsha Bhat <varsha.n.bhat@gmail.com>
2015-11-21 15:14:06 +0800

    Update - 1 (with sample 3-D plot)

--- HealthCeuticals.py
+++ HealthCeuticals.py
@@ -8,7 +8,7 @@ Historical Data
 import random
 import csv
 import matplotlib.pyplot as plt
-
+from mpl_toolkits.mplot3d import Axes3D
 file_name = 'HealthCeuticals.csv'
 file_select = open(file_name)
 data = csv.reader(file_select)
@@ -19,7 +19,7 @@ for row in data:
 	if row[0] == "Month":
 		indicator = "yellow"
 	if indicator == "yellow" and row[0] != "Month" and row[0] != "":
-		Historical_Data.append(row[1])
+		Historical_Data.append(int(row[1]))
 
 file_name = 'HealthCeuticals.csv'
 file_select = open(file_name)
@@ -27,15 +27,12 @@ data = csv.reader(file_select)
 
 
 for row in data:
-	print(row)
 	if row[0] == "Expiration Cost":
 		expiration_cost = row[1]
 	if row[0] == "Air Freight":
 		air_freight = row[1]	
-print(expiration_cost, air_freight)
 
-print(Historical_Data)
-def demand(historical_data):
+def hist_demand(historical_data):
 	""" This function generates random demand values from historical Historical_Data """
 	demand_rand = []
 	min_value = 0
@@ -50,28 +47,41 @@ def avg_demand(rand_demand):
 	sum_demand = 0
 	for demand in rand_demand:
 		sum_demand += int(demand)
-	return int(sum_demand/len(rand_demand)
-random_demand = demand(Historical_Data)
+	return int(sum_demand/len(rand_demand))
+random_demand = hist_demand(Historical_Data)
 
 
+avg_cost = []
+amt_stocked = []
+usr_demand = []
 
-print(random_demand)
-print(avg_demand(random_demand))
 
-# cost = []
-# amt_stocked = []
-# usr_demand = []
-# ##Amount Stocked
-# for i in range(min(Historical_Data), max(Historical_Data)):
-# 	demnd = avg_demand(demand(Historical_Data))
-# 	amt_stocked.append(i)
-# 	usr_demand.append(demand)
+def calc_cost(supply, demand, unit_cost_expiration, unit_cost_restock):
+	if supply == demand:
+		return 0
+	elif supply >  demand:
+		return unit_cost_expiration*(supply-demand)
+	else:
+		return unit_cost_restock*(demand-supply)
 
-# 	if i == demnd:
-# 		cost.append(0)
-# 	elif i > demnd:
-# 		cost.append((i-demnd)*50)
-# 	else:
-# 		cost.append((demnd-i)*150)
-# print(cost,amt_stocked,usr_demand)
+for i in range(1, 10000):
+	amt_stocked.append(i)
+	demnd = avg_demand(hist_demand(Historical_Data))
+	usr_demand.append(demnd)
+	avg_cost.append(calc_cost(i,demnd,50,150))
 
+z = avg_cost
+y = amt_stocked
+x = usr_demand
+
+fig = plt.figure()
+ax = plt.axes(projection='3d')
+
+ax.scatter(x, y, z)
+ax.set_title("Supply Demand Cost Plot")
+ax.set_xlabel("usr_demand")
+ax.set_ylabel("amt_stocked")
+ax.set_zlabel("avg_cost")
+plt.show()
+#ax.plot_surface(x, y, z, cmap=plt.cm.jet, rstride=100, cstride=100, linewidth=100)
+# fig.savefig('test')
